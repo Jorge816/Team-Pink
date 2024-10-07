29e93e3069bb160f9a1b5b44af3d9a020391721e
Jorge816 <127879088+Jorge816@users.noreply.github.com>
2024-10-07 09:17:21 -0400

    Update Helper.java
    Added some extra validation like is positive and if the input is in range.

--- Calculator_TeamPink/src/Helper.java
+++ Calculator_TeamPink/src/Helper.java
@@ -12,18 +12,54 @@
 public class Helper {
 
 
-    public static double monthlyPayment(double loanAmount, double annualInterestRate, int numberOfYears) {
-    // Convert annual interest rate to a monthly rate
-    double monthlyInterestRate = annualInterestRate / 12 / 100;
-    // Total number of payments
-    int numberOfMonths = numberOfYears * 12;
-
-    // Monthly payment calculation using the correct formula
-    double monthlyPayment = (loanAmount * monthlyInterestRate) / 
-                        (1 - (1 / Math.pow((1 + monthlyInterestRate), numberOfMonths)));
-
-    // Return the calculated monthly payment
-    return monthlyPayment;
+    public static boolean isPositiveNumber(String num){
+        try{
+            double val = Double.parseDouble(num);
+            return val > 0;
+        }catch (NumberFormatException e){
+            return false;
+        }
+    }
+    
+    public static boolean inRange(double num, double max){
+        return num <= max;
+    }
+    
+    public static double calcMonthlyPayment(double loanAmount, double annualInterestRate, int years) {
+        String msg = "";
+        
+        if(years <= 0){
+            msg += "Input must be positive. Years must be greater than 0. \n";
+        }
+        
+        if(annualInterestRate <= 0){
+            msg += "Input must be positive. Annual interest rate must be greater than 0. \n";
+        }
+        
+        if(loanAmount <= 0){
+            msg += "Input must be positive. Loan Amount must be greater than 0. \n";
+        }
+        
+        double maxValue = 999999999;
+        if(!inRange(loanAmount, maxValue)){
+            msg += "Input too large: Loan amount exceeds limit.";
+        }
+        
+        if(!inRange(annualInterestRate, maxValue)){
+            msg += "Input too large: Interest rate exceeds limit.";
+        }
+        
+        if(!msg.isEmpty()){
+            System.out.println(msg);
+            return -1;
+        }
+        
+        double monthlyInterestRate = annualInterestRate / 1200;
+        int numMonths = years * 12;
+        double monthlyPayment = (loanAmount * monthlyInterestRate) /
+                (1 - Math.pow(1 + monthlyInterestRate, -numMonths));
+        
+        return monthlyPayment;
     }
     
 }
