e4f7df2b4e768ab8a192c1e91551f1a33a9de44b
Jorge816 <154jorgeestrada@gmail.com>
2024-10-24 11:43:48 -0400

    Update Main.java

--- Calculator-TeamPink/src/Main.java
+++ Calculator-TeamPink/src/Main.java
@@ -4276,7 +4276,6 @@ String aboutMessage = "<html>"
     }//GEN-LAST:event_cashIncentivesInputActionPerformed
 
     private void AutoLoancalcButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AutoLoancalcButtonActionPerformed
-        // TODO add your handling code here:
         String autoPriceValue = autoPriceInput.getText();
         String loanTermValue = loanTermInput.getText();
         String interestRateValue = interestRateInput.getText();
@@ -4286,6 +4285,18 @@ String aboutMessage = "<html>"
         String amtOwnedTradeInValue = amtOwnedInput.getText();
         String salesTaxValue = salesTaxInput.getText();
         String otherFeesValue = otherFeesInput.getText();
+        
+        /*double autoPrice = Double.parseDouble(autoPriceValue);
+        double loanTerm = Double.parseDouble(loanTermValue);
+        double interestRate = Double.parseDouble(interestRateValue);
+        double cashIncentives = Double.parseDouble(cashIncentivesValue);
+        double downPayment = Double.parseDouble(downPaymentValue);
+        double tradeInValue = Double.parseDouble(tradeInValueValue);
+        double amtOwnedTradeIn = Double.parseDouble(amtOwnedTradeInValue);
+        double salesTax = Double.parseDouble(salesTaxValue);
+        double otherFees = Double.parseDouble(otherFeesValue);*/
+        
+        
         
         double[] result = Helper.calcAutoLoan(autoPriceValue, loanTermValue, interestRateValue, cashIncentivesValue, downPaymentValue, tradeInValueValue, amtOwnedTradeInValue, salesTaxValue, otherFeesValue);
         if(result[0] == -1 || result[1] == -1){
@@ -4299,6 +4310,23 @@ String aboutMessage = "<html>"
         }else{
             resultOutput.setText(resultFormat.format(result[0]));
         }
+        
+        /*double paymentWithoutFees = result[0];
+        double paymentWithFees = result[1]; 
+        
+        if(includedFeesAuto.isSelected()){
+            DecimalFormat resultFormat = new DecimalFormat("$#,###.00");
+            resultOutput.setText(resultFormat.format(paymentWithFees));
+        }else{
+            DecimalFormat resultFormat = new DecimalFormat("$#,###.00");
+            resultOutput.setText(resultFormat.format(paymentWithoutFees));
+        }*/
+        
+        //DecimalFormat resultFormat = new DecimalFormat("$#,###.00");
+        //resultOutput.setText(resultFormat.format(result));
+        
+        
+        //includedFeesAutoActionPerformed(evt);
     }//GEN-LAST:event_AutoLoancalcButtonActionPerformed
 
     private void AutoLoanclearButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AutoLoanclearButtonActionPerformed
